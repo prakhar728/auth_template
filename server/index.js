737719f912e0fe46032b594c9be40cc3d1ddd422
Prakhar Ojha <prakharojha12@gmail.com>
2021-08-30 15:23:52 +0530

    BackEnd Done

---
+++ server/index.js
@@ -0,0 +1,29 @@
+//IMPORTING EXPRESS AND CREATING APP INSTANCE
+const express = require('express');
+const app = express();
+
+//TELLING EXPRESS TO USE BODY PARSER
+app.use(express.json());
+
+
+//DOTENV TO USE ENV FILES
+const dotenv = require('dotenv');
+dotenv.config();
+
+//IMPORTING MONGOOSE AND CONNECTING TO THE DATABASE
+const mong = require('mongoose');
+mong.connect(process.env.MONGO_URI,()=>{
+    console.log('Connected to DB!');
+})
+
+//IMPORTING ALL THE ROUTES
+const authRoute = require('./routes/auth');
+
+//USING THE ROUTES IN EXPRESS
+app.use('/api/user',authRoute);
+
+
+//SETTING THE PORT TO LISTEN
+app.listen(3000,()=>{
+    console.log('App live on 3000')
+})
